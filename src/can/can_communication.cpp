009d451845c5ad92d9969edb8b9aa15083213322
Akari <masiro.to.akari@gmail.com>
2022-12-27 13:32:47 +0900

    Refactoring code

--- src/can/can_communication.cpp
+++ src/can/can_communication.cpp
@@ -18,7 +18,7 @@ namespace MaSiRoProject
 {
 namespace CAN
 {
-
+#pragma region ThreadCAN
 ///////////////////////////////////////////////////////////////////
 #define THREAD_NAME_CAN "ThreadCAN"
 CanCommunicationImpl *can;
@@ -108,78 +108,52 @@ void thread_can(void *args)
     }
     flag_thread_can_initialized = false;
 }
+#pragma endregion
 
 ////////////////////////////////////////////////////////////////////////////////////////////
-bool CanCommunication::change_mode(CAN_CTRL_STATE mode = CAN_CTRL_STATE::MODE_UNKNOW)
+
+/////////////////////////////////
+// Constructor
+/////////////////////////////////
+#pragma region Constructor
+CanCommunication::CanCommunication() : interrupt(CAN_COMMUNICATION_PIN_INTERRUPT)
+{
+    can = new CanCommunicationImpl();
+}
+
+CanCommunication::~CanCommunication()
+{
+    flag_thread_can_fin = true;
+}
+#pragma endregion
+
+/////////////////////////////////
+// Setup
+/////////////////////////////////
+#pragma region Setup
+
+bool CanCommunication::begin()
 {
     bool result = true;
-    if (CAN_CTRL_STATE::MODE_UNKNOW == mode) {
-        switch (current_mode) {
-            case CAN_CTRL_STATE::MODE_READY:
-            case CAN_CTRL_STATE::MODE_INACTIVE:
-            case CAN_CTRL_STATE::MODE_ABORT:
-            case CAN_CTRL_STATE::MODE_STOPPING:
-                request_mode = CAN_CTRL_STATE::MODE_RUNNING;
-                break;
-            case CAN_CTRL_STATE::MODE_RUNNING:
-                request_mode = CAN_CTRL_STATE::MODE_INACTIVE;
-                break;
-            case CAN_CTRL_STATE::MODE_PAUSE:
-                request_mode = CAN_CTRL_STATE::MODE_PAUSE;
-                break;
-            case CAN_CTRL_STATE::MODE_FINISHED:
-            case CAN_CTRL_STATE::MODE_NOT_INITIALIZE:
-            default:
-                break;
+    if (false == flag_thread_can_initialized) {
+        flag_thread_can_initialized == true;
+        this->setup_default();
+        if (-1 != interrupt) {
+            pinMode(interrupt, INPUT);
+            attachInterrupt(interrupt, thread_can_on_interrupt, FALLING);
         }
-    } else {
-        request_mode = mode;
+        this->task_assigned_size = 4096;
+        xTaskCreatePinnedToCore(thread_can, //
+                                THREAD_NAME_CAN,
+                                this->task_assigned_size,
+                                NULL,
+                                3,
+                                &this->task_handle,
+                                THREAD_CORE_CAN);
     }
-    flag_thread_can_change_mode = true;
-    if (CAN_CTRL_STATE::MODE_PAUSE == request_mode) {
-        delay(CAN_THREAD_INTERVAL);
-    }
-
     return result;
 }
-bool CanCommunication::request_pause()
-{
-    return this->change_mode(CAN_CTRL_STATE::MODE_PAUSE);
-}
-bool CanCommunication::request_running()
-{
-    return this->change_mode(CAN_CTRL_STATE::MODE_PAUSE);
-}
-////////////////////////////////////////////////////////////////////////////////////////////
 
-CanDeviceInfo CanCommunication::get_device_info()
-{
-    return can->get_device_info();
-}
-
-std::vector<CanData> CanCommunication::get_send_loop()
-{
-    return can->get_loop_shot();
-}
-
-std::vector<CanData> CanCommunication::get_received()
-{
-    return can->get_received();
-}
-std::vector<CanData> CanCommunication::get_resume()
-{
-    return can->get_resume();
-}
-
-bool CanCommunication::setup_callback(void)
-{
-    return true;
-}
-bool CanCommunication::add_resume(CanData data)
-{
-    data.time = 0;
-    return can->add_resume(data, true);
-}
 bool CanCommunication::setup_default(void)
 {
     bool result = true;
@@ -188,31 +162,12 @@ bool CanCommunication::setup_default(void)
     }
     return result;
 }
-bool CanCommunication::add_one_shot(CanData data)
-{
-    return can->add_one_shot(data);
-}
-bool CanCommunication::add_loop_shot(CanData data, int interval)
-{
-    return can->add_loop_shot(data, interval);
-}
+#pragma endregion
 
-bool CanCommunication::clear_loop_shot()
-{
-    return can->clear_loop_shot();
-}
-bool CanCommunication::clear_resume()
-{
-    return can->clear_resume();
-}
-bool CanCommunication::delete_loop_shot(unsigned long Id)
-{
-    return can->delete_loop_shot(Id);
-}
-bool CanCommunication::delete_resume(unsigned long Id)
-{
-    return can->delete_resume(Id);
-}
+/////////////////////////////////
+// Set Callback
+/////////////////////////////////
+#pragma region Callback
 
 bool CanCommunication::set_callback_message(MessageFunction callback)
 {
@@ -264,30 +219,120 @@ bool CanCommunication::set_callback_setting_default(SettingDefaultFunction callb
     }
     return result;
 }
+#pragma endregion
 
-bool CanCommunication::begin()
+/////////////////////////////////
+// Getter / Setter
+/////////////////////////////////
+#pragma region GetterSetter
+CanDeviceInfo CanCommunication::get_device_info()
 {
-    bool result = true;
-    if (false == flag_thread_can_initialized) {
-        flag_thread_can_initialized == true;
-        this->setup_callback();
-        this->setup_default();
-        if (-1 != interrupt) {
-            pinMode(interrupt, INPUT);
-            attachInterrupt(interrupt, thread_can_on_interrupt, FALLING);
-        }
-        this->task_assigned_size = 4096;
-        xTaskCreatePinnedToCore(thread_can, //
-                                THREAD_NAME_CAN,
-                                this->task_assigned_size,
-                                NULL,
-                                3,
-                                &this->task_handle,
-                                THREAD_CORE_CAN);
-    }
-    return result;
+    return can->get_device_info();
 }
 
+bool CanCommunication::change_mode(CAN_CTRL_STATE mode = CAN_CTRL_STATE::MODE_UNKNOW)
+{
+    bool result = true;
+    if (CAN_CTRL_STATE::MODE_UNKNOW == mode) {
+        switch (current_mode) {
+            case CAN_CTRL_STATE::MODE_READY:
+            case CAN_CTRL_STATE::MODE_INACTIVE:
+            case CAN_CTRL_STATE::MODE_ABORT:
+            case CAN_CTRL_STATE::MODE_STOPPING:
+                request_mode = CAN_CTRL_STATE::MODE_RUNNING;
+                break;
+            case CAN_CTRL_STATE::MODE_RUNNING:
+                request_mode = CAN_CTRL_STATE::MODE_INACTIVE;
+                break;
+            case CAN_CTRL_STATE::MODE_PAUSE:
+                request_mode = CAN_CTRL_STATE::MODE_PAUSE;
+                break;
+            case CAN_CTRL_STATE::MODE_FINISHED:
+            case CAN_CTRL_STATE::MODE_NOT_INITIALIZE:
+            default:
+                break;
+        }
+    } else {
+        request_mode = mode;
+    }
+    flag_thread_can_change_mode = true;
+    if (CAN_CTRL_STATE::MODE_PAUSE == request_mode) {
+        delay(CAN_THREAD_INTERVAL);
+    }
+
+    return result;
+}
+#pragma endregion
+
+/////////////////////////////////
+// Getter / Setter for CAN Data
+/////////////////////////////////
+#pragma region GetterSetter_for_CAN_DATA
+std::vector<CanData> CanCommunication::get_send_loop()
+{
+    return can->get_loop_shot();
+}
+
+std::vector<CanData> CanCommunication::get_received()
+{
+    return can->get_received();
+}
+std::vector<CanData> CanCommunication::get_resume()
+{
+    return can->get_resume();
+}
+
+bool CanCommunication::add_one_shot(CanData data)
+{
+    return can->add_one_shot(data);
+}
+bool CanCommunication::add_loop_shot(CanData data, int interval)
+{
+    return can->add_loop_shot(data, interval);
+}
+bool CanCommunication::add_resume(CanData data)
+{
+    data.time = 0;
+    return can->add_resume(data, true);
+}
+
+bool CanCommunication::delete_loop_shot(unsigned long Id)
+{
+    return can->delete_loop_shot(Id);
+}
+bool CanCommunication::delete_resume(unsigned long Id)
+{
+    return can->delete_resume(Id);
+}
+
+bool CanCommunication::clear_loop_shot()
+{
+    return can->clear_loop_shot();
+}
+bool CanCommunication::clear_resume()
+{
+    return can->clear_resume();
+}
+#pragma endregion
+
+/////////////////////////////////
+// Control function
+/////////////////////////////////
+#pragma region Control_function
+bool CanCommunication::request_pause()
+{
+    return this->change_mode(CAN_CTRL_STATE::MODE_PAUSE);
+}
+bool CanCommunication::request_running()
+{
+    return this->change_mode(CAN_CTRL_STATE::MODE_PAUSE);
+}
+#pragma endregion
+
+/////////////////////////////////
+// Debug
+/////////////////////////////////
+#pragma region Debug
 #if DEBUG_MODE
 UBaseType_t CanCommunication::get_stack_size()
 {
@@ -298,17 +343,8 @@ UBaseType_t CanCommunication::get_stack_high_water_mark()
     return uxTaskGetStackHighWaterMark(this->task_handle);
 }
 #endif
+#pragma endregion
 
-////////////////////////////////////////////////////////////////////////////////////////////
-CanCommunication::CanCommunication() : interrupt(CAN_COMMUNICATION_PIN_INTERRUPT)
-{
-    can = new CanCommunicationImpl();
-}
-
-CanCommunication::~CanCommunication()
-{
-    flag_thread_can_fin = true;
-}
 ////////////////////////////////////////////////////////////////////////////////////////////
 } // namespace CAN
 } // namespace MaSiRoProject
--- src/can/can_communication.hpp
+++ src/can/can_communication.hpp
@@ -22,19 +22,38 @@ namespace CAN
 
 class CanCommunication {
 public:
+    /////////////////////////////////
+    // Constructor
+    /////////////////////////////////
     CanCommunication();
     ~CanCommunication();
+
+    /////////////////////////////////
+    // Setup
+    /////////////////////////////////
+    bool begin();
+    bool setup_default(void);
+
+    /////////////////////////////////
+    // Set Callback
+    /////////////////////////////////
     bool set_callback_message(MessageFunction callback);
     bool set_callback_changed_mode(ChangedModeFunction callback);
     bool set_callback_received(GetReceivedFunction callback);
     bool set_callback_sendable(SendEventFunction callback);
     bool set_callback_setting_default(SettingDefaultFunction callback);
 
-public:
-    bool begin();
-
+    /////////////////////////////////
+    // Getter / Setter
+    /////////////////////////////////
 public:
     CanDeviceInfo get_device_info();
+    bool change_mode(CAN_CTRL_STATE mode);
+
+    /////////////////////////////////
+    // Getter / Setter for CAN Data
+    /////////////////////////////////
+public:
     std::vector<CanData> get_send_loop();
     std::vector<CanData> get_received();
     std::vector<CanData> get_resume();
@@ -44,24 +63,30 @@ public:
     bool add_resume(CanData data);
 
     bool delete_loop_shot(unsigned long Id);
-    bool clear_loop_shot();
-
     bool delete_resume(unsigned long Id);
+
+    bool clear_loop_shot();
     bool clear_resume();
 
+    /////////////////////////////////
+    // Control function
+    /////////////////////////////////
 public:
     bool request_pause();
     bool request_running();
-    bool change_mode(CAN_CTRL_STATE mode);
-    bool setup_default(void);
-    bool setup_callback(void);
 
+    /////////////////////////////////
+    // Debug
+    /////////////////////////////////
 #if DEBUG_MODE
 public:
     UBaseType_t get_stack_high_water_mark();
     UBaseType_t get_stack_size();
 #endif
 
+    /////////////////////////////////
+    // Member
+    /////////////////////////////////
 private:
     byte interrupt;
     SettingDefaultFunction callback_setting_default;
